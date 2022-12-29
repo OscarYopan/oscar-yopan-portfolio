ef1137dc6f177115fcc48ef1a06f4a4045999efe
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-12-29 15:44:11 -0300

    --

---
+++ backup/assets/js/oscar.js
@@ -0,0 +1,51 @@
+// *************************** Nav Animation ***********************
+const nav = document.querySelector('.nav')
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+  if (window.scrollY > nav.offsetHeight + 100) {
+    nav.classList.add('active')
+  } else {
+    nav.classList.remove('active')
+  }
+}
+
+//*********************************** ScrollReveal ******************
+// const scrollReveal = ScrollReveal({
+//   origin: 'top',
+//   distance: '30px',
+//   duration: 700,
+//   reset: true
+// })
+
+// scrollReveal.reveal(
+//   `#home .fa-brands, #home .title, #home .subtitle, #home .presentation. #home .button,
+//   #about .title, #about .subtitle, #about .oscar-photo, #about .presentation, #about .button
+//   #services header, #services .card,
+//   #testimonials header, #testimonials .testimonials
+//   #contact .text, #contact .links,
+//   footer .brand, footer .social
+//   `,
+//   { interval: 100 }
+// )
+
+// window.addEventListener('scroll', function () {
+//   changeHeaderWhenScroll()
+//   backToTop()
+// })
+
+//*************************** Height &  Width ************
+// sectionHeight()
+
+// function sectionHeight() {
+//   const height =
+//     window.innerHeight ||
+//     document.documentElement.clientHeight ||
+//     document.body.clientHeight
+
+//   const width =
+//     window.innerWidth ||
+//     document.documentElement.clientWidth ||
+//     document.body.clientWidth
+//     console.log('H: ' + height, ', W: ' + width)
+// }
